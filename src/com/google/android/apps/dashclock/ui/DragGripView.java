AOKP/packages_apps_ROMControl

RC: Add toggle configurations
Change-Id: I52b060c1ec39e4ac30aad4e03da8211a8cb0aaf3
Signed-off-by: Roman Birg <romanbirg@gmail.com>

src/com/google/android/apps/dashclock/ui/DragGripView.java
@@ -1,0 +1,129 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.apps.dashclock.ui;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+
+import com.aokp.romcontrol.R;
+
+public class DragGripView extends View {
+    private static final int[] ATTRS = new int[]{
+            android.R.attr.gravity,
+            android.R.attr.color,
+    };
+
+    private static final int HORIZ_RIDGES = 4;
+
+    private int mGravity = Gravity.START;
+    private int mColor = 0x33333333;
+
+    private Paint mRidgePaint;
+
+    private float mRidgeSize;
+    private float mRidgeGap;
+
+    private int mWidth;
+    private int mHeight;
+
+    public DragGripView(Context context) {
+        this(context, null, 0);
+    }
+
+    public DragGripView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DragGripView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        final TypedArray a = context.obtainStyledAttributes(attrs, ATTRS);
+        mGravity = a.getInteger(0, mGravity);
+        mColor = a.getColor(1, mColor);
+        a.recycle();
+
+        final Resources res = getResources();
+        mRidgeSize = res.getDimensionPixelSize(R.dimen.drag_grip_ridge_size);
+        mRidgeGap = res.getDimensionPixelSize(R.dimen.drag_grip_ridge_gap);
+
+        mRidgePaint = new Paint();
+        mRidgePaint.setColor(mColor);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(
+                View.resolveSize(
+                        (int) (HORIZ_RIDGES * (mRidgeSize + mRidgeGap) - mRidgeGap)
+                                + getPaddingLeft() + getPaddingRight(),
+                        widthMeasureSpec),
+                View.resolveSize(
+                        (int) mRidgeSize,
+                        heightMeasureSpec));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        float drawWidth = HORIZ_RIDGES * (mRidgeSize + mRidgeGap) - mRidgeGap;
+        float drawLeft;
+
+        switch (Gravity.getAbsoluteGravity(mGravity, getLayoutDirection())
+                & Gravity.HORIZONTAL_GRAVITY_MASK) {
+            case Gravity.CENTER_HORIZONTAL:
+                drawLeft = getPaddingLeft()
+                        + ((mWidth - getPaddingLeft() - getPaddingRight()) - drawWidth) / 2;
+                break;
+            case Gravity.RIGHT:
+                drawLeft = getWidth() - getPaddingRight() - drawWidth;
+                break;
+            default:
+                drawLeft = getPaddingLeft();
+        }
+
+        int vertRidges = (int) ((mHeight - getPaddingTop() - getPaddingBottom() + mRidgeGap)
+                / (mRidgeSize + mRidgeGap));
+        float drawHeight = vertRidges * (mRidgeSize + mRidgeGap) - mRidgeGap;
+        float drawTop = getPaddingTop()
+                + ((mHeight - getPaddingTop() - getPaddingBottom()) - drawHeight) / 2;
+
+        for (int y = 0; y < vertRidges; y++) {
+            for (int x = 0; x < HORIZ_RIDGES; x++) {
+                canvas.drawRect(
+                        drawLeft + x * (mRidgeSize + mRidgeGap),
+                        drawTop + y * (mRidgeSize + mRidgeGap),
+                        drawLeft + x * (mRidgeSize + mRidgeGap) + mRidgeSize,
+                        drawTop + y * (mRidgeSize + mRidgeGap) + mRidgeSize,
+                        mRidgePaint);
+            }
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mHeight = h;
+        mWidth = w;
+    }
+}

src/com/aokp/romcontrol/MainActivity.java
@@ -10,13 +10,12 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.aokp.romcontrol.R;
 import com.aokp.romcontrol.fragments.AboutFragment;
 import com.aokp.romcontrol.fragments.GeneralSettingsFragment;
 import com.aokp.romcontrol.fragments.HardwareKeysFragment;
-import com.aokp.romcontrol.fragments.InstallerSettingsFragment;
 import com.aokp.romcontrol.fragments.NavigationDrawerFragment;
 import com.aokp.romcontrol.fragments.StatusbarSettingsFragment;
+import com.aokp.romcontrol.fragments.ToggleSetupFragment;
 
 
 public class MainActivity extends Activity
@@ -82,6 +81,9 @@ public class MainActivity extends Activity
                 fragment = new StatusbarSettingsFragment();
                 break;
 
+            case 4:
+                fragment = new ToggleSetupFragment();
+                break;
             /**case 4:
                 fragment = new InstallerSettingsFragment();
                 break;*/

src/com/aokp/romcontrol/util/EasyPair.java
@@ -1,0 +1,83 @@
+package com.aokp.romcontrol.util;
+
+import android.util.Pair;
+
+import java.util.Arrays;
+
+public class EasyPair<Ta, Tb> extends Pair<Ta, Tb> {
+    /**
+     * Constructor for a Pair. If either are null then equals() and hashCode() will throw
+     * a NullPointerException.
+     *
+     * @param first  the first object in the Pair
+     * @param second the second object in the pair
+     */
+    public Ta first;
+    public Tb second;
+
+    public EasyPair(Ta first, Tb second) {
+        super(first, second);
+        this.first = first;
+        this.second = second;
+    }
+
+    /**
+     * Constructs a new EasyPair from an existing EasyPair
+     *
+     * @param pair
+     */
+    public EasyPair(EasyPair<Ta, Tb> pair) {
+        super(pair.getFirst(), pair.getSecond());
+        this.first = pair.getFirst();
+        this.second = pair.getSecond();
+    }
+
+    @Override
+    public String toString() {
+        String first_;
+        String second_;
+
+        try {
+            first_ = Arrays.toString((Ta[]) first);
+        } catch (ClassCastException badCast) {
+            first_ = first.toString();
+        }
+
+        try {
+            second_ = Arrays.toString((Tb[]) second);
+        } catch (ClassCastException badCast) {
+            second_ = second.toString();
+        }
+        return "EasyPair<" + first_ + ", " + second_ + ">";
+    }
+
+    public Ta getFirst() {
+        return first;
+    }
+
+    public Tb getSecond() {
+        return second;
+    }
+
+    public void changeFirst(Ta newFirst) {
+        this.first = newFirst;
+    }
+
+    public void changeSecond(Tb newSecond) {
+        this.second = newSecond;
+    }
+
+    public EasyPair<Ta, Tb> clonePair() {
+        return new EasyPair<Ta, Tb>(first, second);
+    }
+
+    public boolean equals() {
+        // yea they equal but not in a good way
+        if (first == null || second == null) {
+            return false;
+        }
+        // return a method that won't throw null
+        return first == second;
+    }
+}
+

ReVolt-ROM/packages_apps_ReVoltControl

Conflicts:
	res/values/arrays.xml
	src/com/revolt/control/MainActivity.java

src/com/google/android/apps/dashclock/ui/DragGripView.java
@@ -1,0 +1,129 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.apps.dashclock.ui;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+
+import com.revolt.control.R;
+
+public class DragGripView extends View {
+    private static final int[] ATTRS = new int[]{
+            android.R.attr.gravity,
+            android.R.attr.color,
+    };
+
+    private static final int HORIZ_RIDGES = 4;
+
+    private int mGravity = Gravity.START;
+    private int mColor = 0x33333333;
+
+    private Paint mRidgePaint;
+
+    private float mRidgeSize;
+    private float mRidgeGap;
+
+    private int mWidth;
+    private int mHeight;
+
+    public DragGripView(Context context) {
+        this(context, null, 0);
+    }
+
+    public DragGripView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DragGripView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        final TypedArray a = context.obtainStyledAttributes(attrs, ATTRS);
+        mGravity = a.getInteger(0, mGravity);
+        mColor = a.getColor(1, mColor);
+        a.recycle();
+
+        final Resources res = getResources();
+        mRidgeSize = res.getDimensionPixelSize(R.dimen.drag_grip_ridge_size);
+        mRidgeGap = res.getDimensionPixelSize(R.dimen.drag_grip_ridge_gap);
+
+        mRidgePaint = new Paint();
+        mRidgePaint.setColor(mColor);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(
+                View.resolveSize(
+                        (int) (HORIZ_RIDGES * (mRidgeSize + mRidgeGap) - mRidgeGap)
+                                + getPaddingLeft() + getPaddingRight(),
+                        widthMeasureSpec),
+                View.resolveSize(
+                        (int) mRidgeSize,
+                        heightMeasureSpec));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        float drawWidth = HORIZ_RIDGES * (mRidgeSize + mRidgeGap) - mRidgeGap;
+        float drawLeft;
+
+        switch (Gravity.getAbsoluteGravity(mGravity, getLayoutDirection())
+                & Gravity.HORIZONTAL_GRAVITY_MASK) {
+            case Gravity.CENTER_HORIZONTAL:
+                drawLeft = getPaddingLeft()
+                        + ((mWidth - getPaddingLeft() - getPaddingRight()) - drawWidth) / 2;
+                break;
+            case Gravity.RIGHT:
+                drawLeft = getWidth() - getPaddingRight() - drawWidth;
+                break;
+            default:
+                drawLeft = getPaddingLeft();
+        }
+
+        int vertRidges = (int) ((mHeight - getPaddingTop() - getPaddingBottom() + mRidgeGap)
+                / (mRidgeSize + mRidgeGap));
+        float drawHeight = vertRidges * (mRidgeSize + mRidgeGap) - mRidgeGap;
+        float drawTop = getPaddingTop()
+                + ((mHeight - getPaddingTop() - getPaddingBottom()) - drawHeight) / 2;
+
+        for (int y = 0; y < vertRidges; y++) {
+            for (int x = 0; x < HORIZ_RIDGES; x++) {
+                canvas.drawRect(
+                        drawLeft + x * (mRidgeSize + mRidgeGap),
+                        drawTop + y * (mRidgeSize + mRidgeGap),
+                        drawLeft + x * (mRidgeSize + mRidgeGap) + mRidgeSize,
+                        drawTop + y * (mRidgeSize + mRidgeGap) + mRidgeSize,
+                        mRidgePaint);
+            }
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mHeight = h;
+        mWidth = w;
+    }
+}

src/com/revolt/control/MainActivity.java
@@ -9,13 +9,13 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MenuItem
 import com.revolt.control.R;
 import com.revolt.control.fragments.GeneralSettingsFragment;
 import com.revolt.control.fragments.HardwareKeysFragment;
 import com.revolt.control.fragments.NavigationDrawerFragment;
 import com.revolt.control.fragments.StatusbarSettingsFragment;
-
+import com.revolt.control.fragments.ToggleSetupFragment;
 
 public class MainActivity extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -74,6 +74,10 @@ public class MainActivity extends Activity
 
             case 2:
                 fragment = new StatusbarSettingsFragment();
+                break;
+
+            case 3:
+                fragment = new ToggleSetupFragment();
                 break;
         }
         return fragment;

src/com/revolt/control/util/EasyPair.java
@@ -1,0 +1,83 @@
+package com.revolt.control.util;
+
+import android.util.Pair;
+
+import java.util.Arrays;
+
+public class EasyPair<Ta, Tb> extends Pair<Ta, Tb> {
+    /**
+     * Constructor for a Pair. If either are null then equals() and hashCode() will throw
+     * a NullPointerException.
+     *
+     * @param first  the first object in the Pair
+     * @param second the second object in the pair
+     */
+    public Ta first;
+    public Tb second;
+
+    public EasyPair(Ta first, Tb second) {
+        super(first, second);
+        this.first = first;
+        this.second = second;
+    }
+
+    /**
+     * Constructs a new EasyPair from an existing EasyPair
+     *
+     * @param pair
+     */
+    public EasyPair(EasyPair<Ta, Tb> pair) {
+        super(pair.getFirst(), pair.getSecond());
+        this.first = pair.getFirst();
+        this.second = pair.getSecond();
+    }
+
+    @Override
+    public String toString() {
+        String first_;
+        String second_;
+
+        try {
+            first_ = Arrays.toString((Ta[]) first);
+        } catch (ClassCastException badCast) {
+            first_ = first.toString();
+        }
+
+        try {
+            second_ = Arrays.toString((Tb[]) second);
+        } catch (ClassCastException badCast) {
+            second_ = second.toString();
+        }
+        return "EasyPair<" + first_ + ", " + second_ + ">";
+    }
+
+    public Ta getFirst() {
+        return first;
+    }
+
+    public Tb getSecond() {
+        return second;
+    }
+
+    public void changeFirst(Ta newFirst) {
+        this.first = newFirst;
+    }
+
+    public void changeSecond(Tb newSecond) {
+        this.second = newSecond;
+    }
+
+    public EasyPair<Ta, Tb> clonePair() {
+        return new EasyPair<Ta, Tb>(first, second);
+    }
+
+    public boolean equals() {
+        // yea they equal but not in a good way
+        if (first == null || second == null) {
+            return false;
+        }
+        // return a method that won't throw null
+        return first == second;
+    }
+}
+