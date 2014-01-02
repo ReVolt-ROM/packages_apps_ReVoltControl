AOKP/packages_apps_ROMControl

RC: move toggle settings into a separate tab
In order to use a FragmentTabHost, we have to be
using the support library Activities/Fragments.

Change-Id: Ic10cd59331bc72f314f5baebe28805344445d684
Signed-off-by: Roman Birg <romanbirg@gmail.com>

src/com/aokp/romcontrol/MainActivity.java
@@ -1,26 +1,25 @@
 package com.aokp.romcontrol;
 
 import android.app.ActionBar;
-import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.ComponentName;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.aokp.romcontrol.R;
 import com.aokp.romcontrol.fragments.AboutFragment;
-import com.aokp.romcontrol.fragments.ArrangeTogglesFragment;
 import com.aokp.romcontrol.fragments.GeneralSettingsFragment;
 import com.aokp.romcontrol.fragments.HardwareKeysFragment;
 import com.aokp.romcontrol.fragments.InstallerSettingsFragment;
 import com.aokp.romcontrol.fragments.NavigationDrawerFragment;
 import com.aokp.romcontrol.fragments.StatusbarSettingsFragment;
+import com.aokp.romcontrol.fragments.TogglesTabHostFragment;
 
 
-public class MainActivity extends Activity
+public class MainActivity extends FragmentActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
     /**
@@ -44,19 +43,22 @@ public class MainActivity extends Activity
 
         setContentView(R.layout.activity_main);
         mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getFragmentManager().findFragmentById(R.id.navigation_drawer);
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
 
         // Set up the drawer.
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        ActionBar bar = getActionBar();
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
     }
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
-        FragmentManager fragmentManager = getFragmentManager();
+        FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.container, getFragmentToAttach(position))
                 .commit();
@@ -84,7 +86,7 @@ public class MainActivity extends Activity
                 break;
 
             case 4:
-                fragment = new ArrangeTogglesFragment();
+                fragment = new TogglesTabHostFragment();
                 break;
 
             case 5:

src/com/aokp/romcontrol/fragments/ToggleSettingsFragment.java
@@ -1,0 +1,59 @@
+package com.aokp.romcontrol.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.aokp.romcontrol.R;
+import com.aokp.romcontrol.settings.BaseSetting;
+import com.aokp.romcontrol.settings.BaseSetting.OnSettingChangedListener;
+import com.aokp.romcontrol.settings.SingleChoiceSetting;
+
+/**
+ * Created by roman on 12/30/13.
+ */
+public class ToggleSettingsFragment extends Fragment implements OnSettingChangedListener {
+
+    BaseSetting mTogglesFast, mSwipeToSwitch;
+    SingleChoiceSetting mTogglesPerRow, mToggleStyle, mToggleSide;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_toggle_setup, container, false);
+
+        mTogglesFast = (BaseSetting) v.findViewById(R.id.toggles_fast_toggle);
+        mSwipeToSwitch = (BaseSetting) v.findViewById(R.id.toggles_swipe_to_switch);
+        mTogglesPerRow = (SingleChoiceSetting) v.findViewById(R.id.toggles_per_row);
+        mToggleStyle = (SingleChoiceSetting) v.findViewById(R.id.toggles_style);
+        mToggleSide = (SingleChoiceSetting) v.findViewById(R.id.toggles_fast_side);
+
+        mToggleStyle.setOnSettingChangedListener(this);
+
+
+        return v;
+    }
+
+
+    @Override
+    public void onSettingChanged(String table, String key, String oldValue, String value) {
+        if (table.equals("aokp") && key.equals(mToggleStyle.getKey())) {
+            if (value == null || value.isEmpty()) {
+                // defualt state
+                mTogglesPerRow.setVisibility(View.VISIBLE);
+                mTogglesFast.setVisibility(View.VISIBLE);
+                mToggleSide.setVisibility(View.VISIBLE);
+                mSwipeToSwitch.setVisibility(View.VISIBLE);
+            } else {
+                mTogglesPerRow.setVisibility(value.equals("0" /* 0 is the tile */)
+                        ? View.VISIBLE : View.GONE);
+                mTogglesFast.setVisibility(value.equals("0" /* 0 is the tile */)
+                        ? View.VISIBLE : View.GONE);
+                mToggleSide.setVisibility(value.equals("0" /* 0 is the tile */)
+                        ? View.VISIBLE : View.GONE);
+                mSwipeToSwitch.setVisibility(value.equals("0" /* 0 is the tile */)
+                        ? View.VISIBLE : View.GONE);
+            }
+        }
+    }
+}

ReVolt-ROM/packages_apps_ReVoltControl

Conflicts:
	AndroidManifest.xml
	res/layout/fragment_configure_toggles.xml
	res/layout/fragment_toggle_setup.xml
	src/com/revolt/control/MainActivity.java
	src/com/revolt/control/fragments/ArrangeTogglesFragment.java

src/com/revolt/control/MainActivity.java
@@ -15,6 +15,7 @@ import com.revolt.control.fragments.GeneralSettingsFragment;
 import com.revolt.control.fragments.HardwareKeysFragment;
 import com.revolt.control.fragments.NavigationDrawerFragment;
 import com.revolt.control.fragments.StatusbarSettingsFragment;
+import com.revolt.control.fragments.TogglesTabHostFragment;
 
 public class MainActivity extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -47,6 +48,9 @@ public class MainActivity extends Activity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        ActionBar bar = getActionBar();
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
     }
 
     @Override
@@ -76,7 +80,7 @@ public class MainActivity extends Activity
                 break;
 
             case 3:
-                fragment = new ArrangeTogglesFragment();
+                fragment = new TogglesTabHostFragment();
                 break;
         }
         return fragment;

src/com/revolt/control/fragments/ToggleSettingsFragment.java
@@ -1,0 +1,63 @@
+package com.revolt.control.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.revolt.control.R;
+import com.revolt.control.settings.BaseSetting;
+import com.revolt.control.settings.BaseSetting.OnSettingChangedListener;
+import com.revolt.control.settings.SingleChoiceSetting;
+
+/**
+ * Created by roman on 12/30/13.
+ */
+public class ToggleSettingsFragment extends Fragment implements OnSettingChangedListener {
+
+    BaseSetting mTogglesFast, mSwipeToSwitch;
+    SingleChoiceSetting mTogglesPerRow, mToggleStyle, mToggleSide;
+
+    public ToggleSettingsFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_toggle_setup, container, false);
+
+        mTogglesFast = (BaseSetting) v.findViewById(R.id.toggles_fast_toggle);
+        mSwipeToSwitch = (BaseSetting) v.findViewById(R.id.toggles_swipe_to_switch);
+        mTogglesPerRow = (SingleChoiceSetting) v.findViewById(R.id.toggles_per_row);
+        mToggleStyle = (SingleChoiceSetting) v.findViewById(R.id.toggles_style);
+        mToggleSide = (SingleChoiceSetting) v.findViewById(R.id.toggles_fast_side);
+
+        mToggleStyle.setOnSettingChangedListener(this);
+
+
+        return v;
+    }
+
+
+    @Override
+    public void onSettingChanged(String table, String key, String oldValue, String value) {
+        if (table.equals("revolt") && key.equals(mToggleStyle.getKey())) {
+            if (value == null || value.isEmpty()) {
+                // defualt state
+                mTogglesPerRow.setVisibility(View.VISIBLE);
+                mTogglesFast.setVisibility(View.VISIBLE);
+                mToggleSide.setVisibility(View.VISIBLE);
+                mSwipeToSwitch.setVisibility(View.VISIBLE);
+            } else {
+                mTogglesPerRow.setVisibility(value.equals("0" /* 0 is the tile */)
+                        ? View.VISIBLE : View.GONE);
+                mTogglesFast.setVisibility(value.equals("0" /* 0 is the tile */)
+                        ? View.VISIBLE : View.GONE);
+                mToggleSide.setVisibility(value.equals("0" /* 0 is the tile */)
+                        ? View.VISIBLE : View.GONE);
+                mSwipeToSwitch.setVisibility(value.equals("0" /* 0 is the tile */)
+                        ? View.VISIBLE : View.GONE);
+            }
+        }
+    }
+}