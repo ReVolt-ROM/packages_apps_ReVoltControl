AOKP/packages_apps_ROMControl

RC: Power Menu customizations
Change-Id: I4a440c213ad761f57ba51054f2e7539651a9b660
Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

src/com/aokp/romcontrol/MainActivity.java
@@ -26,17 +26,20 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+
 import com.aokp.romcontrol.fragments.GeneralSettingsFragment;
 import com.aokp.romcontrol.fragments.HardwareKeysFragment;
 import com.aokp.romcontrol.fragments.InstallerSettingsFragment;
 import com.aokp.romcontrol.fragments.LockscreenSettingsFragment;
 import com.aokp.romcontrol.fragments.NavRingTargets;
 import com.aokp.romcontrol.fragments.NavigationDrawerFragment;
+import com.aokp.romcontrol.fragments.PowerMenuSettingsFragment;
 import com.aokp.romcontrol.fragments.StatusbarSettingsFragment;
+import com.aokp.romcontrol.fragments.SoundSettingsFragment;
 import com.aokp.romcontrol.fragments.about.AboutTabHostFragment;
 import com.aokp.romcontrol.fragments.navbar.NavbarTabHostFragment;
 import com.aokp.romcontrol.fragments.toggles.TogglesTabHostFragment;
-import com.aokp.romcontrol.fragments.SoundSettingsFragment;
+
 
 public class MainActivity extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -113,18 +116,22 @@ public class MainActivity extends Activity
                 break;
 
             case 6:
-                fragment = new NavbarTabHostFragment();
+                fragment = new PowerMenuSettingsFragment();
                 break;
 
             case 7:
-                fragment = new NavRingTargets();
+                fragment = new NavbarTabHostFragment();
                 break;
 
             case 8:
-                fragment = new SoundSettingsFragment();
+                fragment = new NavRingTargets();
                 break;
 
             case 9:
+                fragment = new SoundSettingsFragment();
+                break;
+
+            case 10:
                 fragment = new InstallerSettingsFragment();
                 break;
         }

src/com/aokp/romcontrol/fragments/PowerMenuSettingsFragment.java
@@ -1,0 +1,38 @@
+/*
+* Copyright (C) 2013 The Android Open Kang Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.aokp.romcontrol.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.aokp.romcontrol.R;
+
+public class PowerMenuSettingsFragment extends Fragment {
+
+    public PowerMenuSettingsFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_powermenu_settings, container, false);
+
+        return v;
+    }
+}

ReVolt-ROM/packages_apps_ReVoltControl

Conflicts:
	res/values/arrays.xml
	src/com/revolt/control/MainActivity.java

src/com/revolt/control/MainActivity.java
@@ -16,6 +16,7 @@ import com.revolt.control.fragments.HardwareKeysFragment;
 import com.revolt.control.fragments.LockscreenSettingsFragment;
 import com.revolt.control.fragments.NavRingTargets;
 import com.revolt.control.fragments.NavigationDrawerFragment;
+import com.revolt.control.fragments.PowerMenuSettingsFragment;
 import com.revolt.control.fragments.StatusbarSettingsFragment;
 import com.revolt.control.fragments.navbar.NavbarTabHostFragment;
 import com.revolt.control.fragments.toggles.TogglesTabHostFragment;
@@ -96,14 +97,18 @@ public class MainActivity extends Activity
                 break;
 
             case 6:
-                fragment = new NavbarTabHostFragment();
+                fragment = new PowerMenuSettingsFragment();
                 break;
 
             case 7:
-                fragment = new NavRingTargets();
+                fragment = new NavbarTabHostFragment();
                 break;
 
             case 8:
+                fragment = new NavRingTargets();
+                break;
+
+            case 9:
                 fragment = new SoundSettingsFragment();
                 break;
         }

src/com/revolt/control/fragments/PowerMenuSettingsFragment.java
@@ -1,0 +1,38 @@
+/*
+* Copyright (C) 2013 The Android Open Kang Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.revolt.control.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.revolt.control.R;
+
+public class PowerMenuSettingsFragment extends Fragment {
+
+    public PowerMenuSettingsFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_powermenu_settings, container, false);
+
+        return v;
+    }
+}