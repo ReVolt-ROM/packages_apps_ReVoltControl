AOKP/packages_apps_ROMControl

Feat: Allow users to disable safe headset volume warning (2/2)
Change-Id: Iebd2b04a47d241cd06202fcdbd12de10b29768ca
Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

src/com/aokp/romcontrol/MainActivity.java
@@ -36,6 +36,7 @@ import com.aokp.romcontrol.fragments.StatusbarSettingsFragment;
 import com.aokp.romcontrol.fragments.about.AboutTabHostFragment;
 import com.aokp.romcontrol.fragments.navbar.NavbarTabHostFragment;
 import com.aokp.romcontrol.fragments.toggles.TogglesTabHostFragment;
+import com.aokp.romcontrol.fragments.SoundSettingsFragment;
 
 public class MainActivity extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -120,6 +121,10 @@ public class MainActivity extends Activity
                 break;
 
             case 8:
+                fragment = new SoundSettingsFragment();
+                break;
+
+            case 9:
                 fragment = new InstallerSettingsFragment();
                 break;
         }

src/com/aokp/romcontrol/fragments/SoundSettingsFragment.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2013 The Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aokp.romcontrol.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.aokp.romcontrol.R;
+
+public class SoundSettingsFragment extends Fragment {
+
+    public SoundSettingsFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_sound_settings, container, false);
+
+        return v;
+    }
+}

ReVolt-ROM/packages_apps_ReVoltControl

Conflicts:
	res/values/arrays.xml
	src/com/revolt/control/MainActivity.java

src/com/revolt/control/MainActivity.java
@@ -10,14 +10,16 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.revolt.control.fragments.GeneralSettingsFragment;
+
+import com.revoltcontrol.fragments.GeneralSettingsFragment;
 import com.revolt.control.fragments.HardwareKeysFragment;
 import com.revolt.control.fragments.LockscreenSettingsFragment;
 import com.revolt.control.fragments.NavRingTargets;
 import com.revolt.control.fragments.NavigationDrawerFragment;
 import com.revolt.control.fragments.StatusbarSettingsFragment;
-import com.revolt.control.fragments.toggles.TogglesTabHostFragment;
 import com.revolt.control.fragments.navbar.NavbarTabHostFragment;
+import com.revolt.control.fragments.toggles.TogglesTabHostFragment;
+import com.revolt.control.fragments.SoundSettingsFragment;
 
 public class MainActivity extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -99,6 +101,10 @@ public class MainActivity extends Activity
 
             case 7:
                 fragment = new NavRingTargets();
+                break;
+
+            case 8:
+                fragment = new SoundSettingsFragment();
                 break;
         }
         return fragment;

src/com/revolt/control/fragments/SoundSettingsFragment.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2013 The Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.revolt.control.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.revolt.control.R;
+
+public class SoundSettingsFragment extends Fragment {
+
+    public SoundSettingsFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_sound_settings, container, false);
+
+        return v;
+    }
+}