AOKP/packages_apps_ROMControl

Allows setting of some system animations.
Current List:
Activity Open
Activity Close
Task Open
Task Close
Task Move to front
Task Move to back
Window Open
Window Close
Window Intra Open
Window Intra Close

Duration Control

Prevent overridePendingTransition option

Change-Id: I48a5b1589a42c3de8f3549f6396751a9f08d4614
Signed-off-by: Steve Spear <steve.spear@openpeak.com>

src/com/aokp/romcontrol/MainActivity.java
@@ -31,6 +31,7 @@ import android.view.MenuItem;
 import com.aokp.romcontrol.fragments.about.AboutTabHostFragment;
 import android.view.View;
 import com.aokp.romcontrol.fragments.about.AboutFragment;
+import com.aokp.romcontrol.fragments.AnimationsFragment;
 import com.aokp.romcontrol.fragments.GeneralSettingsFragment;
 import com.aokp.romcontrol.fragments.HardwareKeysFragment;
 import com.aokp.romcontrol.fragments.InstallerSettingsFragment;
@@ -136,6 +137,9 @@ public class MainActivity extends Activity
                 break;
             case 10:
                 fragment = new RibbonsFragment();
+                break;
+            case 11:
+                fragment = new AnimationsFragment();
                 break;
         }
         return fragment;

src/com/aokp/romcontrol/fragments/AnimationsFragment.java
@@ -1,0 +1,146 @@
+package com.aokp.romcontrol.fragments;
+
+import net.margaritov.preference.colorpicker.ColorPickerDialog;
+import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Bitmap.Config;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import com.android.internal.util.aokp.AokpRibbonHelper;
+import com.android.internal.util.aokp.AwesomeAnimationHelper;
+import com.android.internal.util.aokp.AwesomeConstants;
+import com.android.internal.util.aokp.NavRingHelpers;
+import com.aokp.romcontrol.R;
+import com.aokp.romcontrol.settings.CheckboxSetting;
+import com.aokp.romcontrol.settings.ColorPickerSetting;
+import com.aokp.romcontrol.settings.SingleChoiceSetting;
+import net.margaritov.preference.colorpicker.ColorPickerDialog.OnColorChangedListener;
+
+public class AnimationsFragment extends Fragment implements OnSeekBarChangeListener {
+    private SingleChoiceSetting mActivityOpen;
+    private SingleChoiceSetting mActivityClose;
+    private SingleChoiceSetting mTaskOpen;
+    private SingleChoiceSetting mTaskClose;
+    private SingleChoiceSetting mWallpaperOpen;
+    private SingleChoiceSetting mWallpaperClose;
+    private SingleChoiceSetting mWallpaperIntraOpen;
+    private SingleChoiceSetting mWallpaperIntraClose;
+    private SingleChoiceSetting mTaskMoveFront;
+    private SingleChoiceSetting mTaskMoveBack;
+    private SeekBar mDuration;
+    private Context mContext;
+    private int mSeekBarProgress;
+
+    private int[] mAnimations;
+    private String[] mAnimationsStrings;
+    private String[] mAnimationsNum;
+    
+    public AnimationsFragment() {
+    }
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get NavRing Actions
+        mContext = getActivity();
+        
+        mAnimations = AwesomeAnimationHelper.getAnimationsList();
+        int animqty = mAnimations.length;
+        mAnimationsStrings = new String[animqty];
+        mAnimationsNum = new String[animqty];
+        for (int i = 0; i < animqty; i++) {
+            mAnimationsStrings[i] = AwesomeAnimationHelper.getProperName(mContext, mAnimations[i]);
+            mAnimationsNum[i] = String.valueOf(mAnimations[i]);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedinstanceState) {
+        View main = inflater.inflate(R.layout.fragment_animation_settings, container, false);
+
+        mActivityOpen = (SingleChoiceSetting) main.findViewById(R.id.activity_open);
+        mActivityOpen.setEntryValues(mAnimationsNum);
+        mActivityOpen.setEntries(mAnimationsStrings);
+        mActivityOpen.updateSummary();
+
+        mActivityClose = (SingleChoiceSetting) main.findViewById(R.id.activity_close);
+        mActivityClose.setEntryValues(mAnimationsNum);
+        mActivityClose.setEntries(mAnimationsStrings);
+        mActivityClose.updateSummary();
+        
+        mTaskOpen = (SingleChoiceSetting) main.findViewById(R.id.task_open);
+        mTaskOpen.setEntryValues(mAnimationsNum);
+        mTaskOpen.setEntries(mAnimationsStrings);
+        mTaskOpen.updateSummary();
+        
+        mTaskClose = (SingleChoiceSetting) main.findViewById(R.id.task_close);
+        mTaskClose.setEntryValues(mAnimationsNum);
+        mTaskClose.setEntries(mAnimationsStrings);
+        mTaskClose.updateSummary();
+        
+        mTaskMoveFront = (SingleChoiceSetting) main.findViewById(R.id.task_move_to_front);
+        mTaskMoveFront.setEntryValues(mAnimationsNum);
+        mTaskMoveFront.setEntries(mAnimationsStrings);
+        mTaskMoveFront.updateSummary();
+        
+        mTaskMoveBack = (SingleChoiceSetting) main.findViewById(R.id.task_move_to_back);
+        mTaskMoveBack.setEntryValues(mAnimationsNum);
+        mTaskMoveBack.setEntries(mAnimationsStrings);
+        mTaskMoveBack.updateSummary();
+        
+        mWallpaperOpen = (SingleChoiceSetting) main.findViewById(R.id.wallpaper_open);
+        mWallpaperOpen.setEntryValues(mAnimationsNum);
+        mWallpaperOpen.setEntries(mAnimationsStrings);
+        mWallpaperOpen.updateSummary();
+        
+        mWallpaperClose = (SingleChoiceSetting) main.findViewById(R.id.wallpaper_close);
+        mWallpaperClose.setEntryValues(mAnimationsNum);
+        mWallpaperClose.setEntries(mAnimationsStrings);
+        mWallpaperClose.updateSummary();
+        
+        mWallpaperIntraClose = (SingleChoiceSetting) main.findViewById(R.id.wallpaper_intra_close);
+        mWallpaperIntraClose.setEntryValues(mAnimationsNum);
+        mWallpaperIntraClose.setEntries(mAnimationsStrings);
+        mWallpaperIntraClose.updateSummary();
+        
+        mWallpaperIntraOpen = (SingleChoiceSetting) main.findViewById(R.id.wallpaper_intra_open);
+        mWallpaperIntraOpen.setEntryValues(mAnimationsNum);
+        mWallpaperIntraOpen.setEntries(mAnimationsStrings);
+        mWallpaperIntraOpen.updateSummary();
+        
+        mDuration = (SeekBar) main.findViewById(R.id.animation_duration);
+        mDuration.setProgress(Settings.AOKP.getInt(mContext.getContentResolver(), Settings.AOKP.ANIMATION_CONTROLS_DURATION, 50));
+        mDuration.setOnSeekBarChangeListener(this);
+        
+        return main;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        mSeekBarProgress = progress;
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        if (seekBar == mDuration) {
+            Settings.AOKP.putInt(mContext.getContentResolver(), Settings.AOKP.ANIMATION_CONTROLS_DURATION, mSeekBarProgress);
+        }
+        
+    }
+}

ReVolt-ROM/packages_apps_ReVoltControl

Conflicts:
	res/values/arrays.xml
	src/com/revolt/control/MainActivity.java

src/com/revolt/control/MainActivity.java
@@ -11,7 +11,9 @@ import android.provider.Settings;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.revolt.control.fragments.AnimationsFragment;
 import com.revolt.control.fragments.GeneralSettingsFragment;
 import com.revolt.control.fragments.HardwareKeysFragment;
 import com.revolt.control.fragments.LockscreenSettingsFragment;
@@ -117,6 +119,10 @@ public class MainActivity extends Activity
 
             case 10:
                 fragment = new RibbonsFragment();
+                break;
+
+            case 11:
+                fragment = new AnimationsFragment();
                 break;
         }
         return fragment;

src/com/revolt/control/fragments/AnimationsFragment.java
@@ -1,0 +1,145 @@
+package com.revolt.control.fragments;
+
+import net.margaritov.preference.colorpicker.ColorPickerDialog;
+import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Bitmap.Config;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import com.android.internal.util.aokp.AokpRibbonHelper;
+import com.android.internal.util.aokp.AwesomeAnimationHelper;
+import com.android.internal.util.aokp.AwesomeConstants;
+import com.android.internal.util.aokp.NavRingHelpers;
+import com.revoltcontrol.R;
+import com.revolt.control.settings.CheckboxSetting;
+import com.revolt.control.settings.ColorPickerSetting;
+import com.revolt.control.settings.SingleChoiceSetting;
+import net.margaritov.preference.colorpicker.ColorPickerDialog.OnColorChangedListener;
+
+public class AnimationsFragment extends Fragment implements OnSeekBarChangeListener {
+    private SingleChoiceSetting mActivityOpen;
+    private SingleChoiceSetting mActivityClose;
+    private SingleChoiceSetting mTaskOpen;
+    private SingleChoiceSetting mTaskClose;
+    private SingleChoiceSetting mWallpaperOpen;
+    private SingleChoiceSetting mWallpaperClose;
+    private SingleChoiceSetting mWallpaperIntraOpen;
+    private SingleChoiceSetting mWallpaperIntraClose;
+    private SingleChoiceSetting mTaskMoveFront;
+    private SingleChoiceSetting mTaskMoveBack;
+    private SeekBar mDuration;
+    private Context mContext;
+    private int mSeekBarProgress;
+
+    private int[] mAnimations;
+    private String[] mAnimationsStrings;
+    private String[] mAnimationsNum;
+
+    public AnimationsFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get NavRing Actions
+        mContext = getActivity();
+
+        mAnimations = AwesomeAnimationHelper.getAnimationsList();
+        int animqty = mAnimations.length;
+        mAnimationsStrings = new String[animqty];
+        mAnimationsNum = new String[animqty];
+        for (int i = 0; i < animqty; i++) {
+            mAnimationsStrings[i] = AwesomeAnimationHelper.getProperName(mContext, mAnimations[i]);
+            mAnimationsNum[i] = String.valueOf(mAnimations[i]);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedinstanceState) {
+        View main = inflater.inflate(R.layout.fragment_animation_settings, container, false);
+
+        mActivityOpen = (SingleChoiceSetting) main.findViewById(R.id.activity_open);
+        mActivityOpen.setEntryValues(mAnimationsNum);
+        mActivityOpen.setEntries(mAnimationsStrings);
+        mActivityOpen.updateSummary();
+
+        mActivityClose = (SingleChoiceSetting) main.findViewById(R.id.activity_close);
+        mActivityClose.setEntryValues(mAnimationsNum);
+        mActivityClose.setEntries(mAnimationsStrings);
+        mActivityClose.updateSummary();
+
+        mTaskOpen = (SingleChoiceSetting) main.findViewById(R.id.task_open);
+        mTaskOpen.setEntryValues(mAnimationsNum);
+        mTaskOpen.setEntries(mAnimationsStrings);
+        mTaskOpen.updateSummary();
+
+        mTaskClose = (SingleChoiceSetting) main.findViewById(R.id.task_close);
+        mTaskClose.setEntryValues(mAnimationsNum);
+        mTaskClose.setEntries(mAnimationsStrings);
+        mTaskClose.updateSummary();
+
+        mTaskMoveFront = (SingleChoiceSetting) main.findViewById(R.id.task_move_to_front);
+        mTaskMoveFront.setEntryValues(mAnimationsNum);
+        mTaskMoveFront.setEntries(mAnimationsStrings);
+        mTaskMoveFront.updateSummary();
+
+        mTaskMoveBack = (SingleChoiceSetting) main.findViewById(R.id.task_move_to_back);
+        mTaskMoveBack.setEntryValues(mAnimationsNum);
+        mTaskMoveBack.setEntries(mAnimationsStrings);
+        mTaskMoveBack.updateSummary();
+
+        mWallpaperOpen = (SingleChoiceSetting) main.findViewById(R.id.wallpaper_open);
+        mWallpaperOpen.setEntryValues(mAnimationsNum);
+        mWallpaperOpen.setEntries(mAnimationsStrings);
+        mWallpaperOpen.updateSummary();
+
+        mWallpaperClose = (SingleChoiceSetting) main.findViewById(R.id.wallpaper_close);
+        mWallpaperClose.setEntryValues(mAnimationsNum);
+        mWallpaperClose.setEntries(mAnimationsStrings);
+        mWallpaperClose.updateSummary();
+
+        mWallpaperIntraClose = (SingleChoiceSetting) main.findViewById(R.id.wallpaper_intra_close);
+        mWallpaperIntraClose.setEntryValues(mAnimationsNum);
+        mWallpaperIntraClose.setEntries(mAnimationsStrings);
+        mWallpaperIntraClose.updateSummary();
+
+        mWallpaperIntraOpen = (SingleChoiceSetting) main.findViewById(R.id.wallpaper_intra_open);
+        mWallpaperIntraOpen.setEntryValues(mAnimationsNum);
+        mWallpaperIntraOpen.setEntries(mAnimationsStrings);
+        mWallpaperIntraOpen.updateSummary();
+
+        mDuration = (SeekBar) main.findViewById(R.id.animation_duration);
+        mDuration.setProgress(Settings.REVOLT.getInt(mContext.getContentResolver(), Settings.REVOLT.ANIMATION_CONTROLS_DURATION, 50));
+        mDuration.setOnSeekBarChangeListener(this);
+
+        return main;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        mSeekBarProgress = progress;
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        if (seekBar == mDuration) {
+            Settings.REVOLT.putInt(mContext.getContentResolver(), Settings.REVOLT.ANIMATION_CONTROLS_DURATION, mSeekBarProgress);
+        }
+    }
+}