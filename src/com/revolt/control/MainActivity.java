AOKP/packages_apps_ROMControl

Add option to disable charging indicator led
Change-Id: I939614c57332a81e4b10d530ca233a7f2aec9945
Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

src/com/aokp/romcontrol/MainActivity.java
@@ -34,6 +34,7 @@ import com.aokp.romcontrol.fragments.AnimationsFragment;
 import com.aokp.romcontrol.fragments.GeneralSettingsFragment;
 import com.aokp.romcontrol.fragments.HardwareKeysFragment;
 import com.aokp.romcontrol.fragments.InstallerSettingsFragment;
+import com.aokp.romcontrol.fragments.LedSettingsFragment;
 import com.aokp.romcontrol.fragments.LockscreenSettingsFragment;
 import com.aokp.romcontrol.fragments.NavRingTargets;
 import com.aokp.romcontrol.fragments.NavigationDrawerFragment;
@@ -147,6 +148,10 @@ public class MainActivity extends Activity
 
             case 12:
                 fragment = new AnimationsFragment();
+                break;
+
+            case 13:
+                fragment = new LedSettingsFragment();
                 break;
         }
         return fragment;

src/com/aokp/romcontrol/fragments/LedSettingsFragment.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2013 The Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aokp.romcontrol.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.aokp.romcontrol.R;
+
+public class LedSettingsFragment extends Fragment {
+
+    public LedSettingsFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_led_settings, container, false);
+
+        return v;
+    }
+}

ReVolt-ROM/packages_apps_ReVoltControl

Conflicts:
	res/values/arrays.xml
	src/com/revolt/control/MainActivity.java

src/com/revolt/control/MainActivity.java
@@ -17,6 +17,7 @@ import com.revolt.control.fragments.about.AboutTabHostFragment;
 import com.revolt.control.fragments.AnimationsFragment;
 import com.revolt.control.fragments.GeneralSettingsFragment;
 import com.revolt.control.fragments.HardwareKeysFragment;
+import com.revolt.control.fragments.LedSettingsFragment;
 import com.revolt.control.fragments.LockscreenSettingsFragment;
 import com.revolt.control.fragments.NavRingTargets;
 import com.revolt.control.fragments.NavigationDrawerFragment;
@@ -124,6 +125,10 @@ public class MainActivity extends Activity
 
             case 11:
                 fragment = new AnimationsFragment();
+                break;
+
+            case 12:
+                fragment = new LedSettingsFragment();
                 break;
         }
         return fragment;

src/com/revolt/control/fragments/LedSettingsFragment.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2013 The Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.revolt.control.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.revolt.control.R;
+
+public class LedSettingsFragment extends Fragment {
+
+    public LedSettingsFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_led_settings, container, false);
+
+        return v;
+    }
+}