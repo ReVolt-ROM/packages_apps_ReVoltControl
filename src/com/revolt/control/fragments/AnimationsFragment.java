AOKP/packages_apps_ROMControl

RC: make animations faster by default
Having the Animation Duration at 50 makes the entire sytem feel
a bit sluggish.
Half it to make the system animations twice as fast.

Change-Id: I93f3a3921bf5a25465c06d7b128162776b7eb67a
Signed-off-by: Andrew Warfield <hiasant@gmail.com>

src/com/aokp/romcontrol/fragments/AnimationsFragment.java
@@ -120,7 +120,7 @@ public class AnimationsFragment extends Fragment implements OnSeekBarChangeListe
         mWallpaperIntraOpen.updateSummary();
 
         mDuration = (SeekBar) main.findViewById(R.id.animation_duration);
-        mDuration.setProgress(Settings.AOKP.getInt(mContext.getContentResolver(), Settings.AOKP.ANIMATION_CONTROLS_DURATION, 50));
+        mDuration.setProgress(Settings.AOKP.getInt(mContext.getContentResolver(), Settings.AOKP.ANIMATION_CONTROLS_DURATION, 25));
         mDuration.setOnSeekBarChangeListener(this);
 
         return main;

ReVolt-ROM/packages_apps_ReVoltControl

Conflicts:
	src/com/revolt/control/fragments/AnimationsFragment.java

src/com/revolt/control/fragments/AnimationsFragment.java
@@ -120,7 +120,8 @@ public class AnimationsFragment extends Fragment implements OnSeekBarChangeListe
         mWallpaperIntraOpen.updateSummary();
 
         mDuration = (SeekBar) main.findViewById(R.id.animation_duration);
-        mDuration.setProgress(Settings.REVOLT.getInt(mContext.getContentResolver(), Settings.REVOLT.ANIMATION_CONTROLS_DURATION, 50));
+        mDuration.setProgress(Settings.REVOLT.getInt(mContext.getContentResolver(), Settings.REVOLT.ANIMATION_CONTROLS_DURATION, 25));
+
         mDuration.setOnSeekBarChangeListener(this);
 
         return main;