2dcea8a4b6ea8cde048a2abedc512bdd229f4f82
Michael Bestas <mikeioannina@gmail.com>
2014-07-08 18:23:11 +0300

    Add option to force high-end graphics on low memory devices
    Change-Id: Ie80dbf658d6ca4c4fb230482745029098fe6777d
    
    Conflicts:
    
    	AndroidManifest.xml
    	res/layout/fragment_general_settings.xml
    	res/values/strings.xml
    	src/com/aokp/romcontrol/fragments/GeneralSettingsFragment.java

--- src/com/revolt/control/fragments/GeneralSettingsFragment.java
+++ src/com/revolt/control/fragments/GeneralSettingsFragment.java
@@ -1,13 +1,37 @@
+/*
+ * Copyright (C) 2013-2014 The Android Open Kang Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.revolt.control.fragments;
 
+import android.app.ActivityManager;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.SystemProperties;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import com.revolt.control.R;
+import com.revolt.control.settings.CheckboxSetting;
 
 public class GeneralSettingsFragment extends Fragment {
+
+    private static final String FORCE_HIGHEND_GFX_PERSIST_PROP = "persist.sys.force_highendgfx";
+
+    CheckboxSetting mForceHighEndGfx;
 
     public GeneralSettingsFragment() {
 
@@ -16,6 +40,21 @@ public class GeneralSettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_general_settings, container, false);
+
+        mForceHighEndGfx = (CheckboxSetting) v.findViewById(R.id.setting_force_highend_gfx);
+        if (ActivityManager.isLowRamDeviceStatic()) {
+            String forceHighendGfx = SystemProperties.get(FORCE_HIGHEND_GFX_PERSIST_PROP, "false");
+            mForceHighEndGfx.setChecked("true".equals(forceHighendGfx));
+            mForceHighEndGfx.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    SystemProperties.set(FORCE_HIGHEND_GFX_PERSIST_PROP,
+                            mForceHighEndGfx.isChecked() ? "true" : "false");
+                }
+            });
+        } else {
+            mForceHighEndGfx.setVisibility(View.GONE);
+        }
 
         return v;
     }
